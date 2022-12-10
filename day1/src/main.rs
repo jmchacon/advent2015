99e3a3100db44dc5c441e2ff8f2dc03485515218
James Chacon <chacon.james@gmail.com>
2022-12-10 14:55:45 -0800

    day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,5 @@
 //! day1 advent 2022
 use color_eyre::eyre::Result;
-use slab_tree::tree::TreeBuilder;
-use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
@@ -13,6 +11,30 @@ fn main() -> Result<()> {
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
     for (line_num, line) in lines.iter().enumerate() {
+        let mut floor = 0;
+        let mut found = false;
+        for (pos, e) in line.bytes().enumerate() {
+            match e {
+                b'(' => {
+                    floor += 1;
+                }
+                b')' => {
+                    floor -= 1;
+                }
+                _ => {
+                    panic!(
+                        "{} - bad line {line} at postion {} - {e}",
+                        line_num + 1,
+                        pos + 1
+                    );
+                }
+            }
+            if !found && floor == -1 {
+                found = true;
+                println!("{} first floor -1 pos", pos + 1);
+            }
+        }
+        println!("Ended on {floor}");
     }
 
     Ok(())
