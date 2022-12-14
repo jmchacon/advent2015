bd47c7bf7a78055bd6df9cdbc39503259596b673
James Chacon <chacon.james@gmail.com>
2022-12-14 00:54:13 +0000

    day9 part1
    Generate all permutations of routes and then iterate to find the min.

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,7 +1,7 @@
 //! day9 advent 2022
 use clap::Parser;
 use color_eyre::eyre::Result;
-use slab_tree::tree::TreeBuilder;
+use itertools::Itertools;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io;
@@ -23,7 +23,37 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    let mut hm: HashMap<String, HashMap<String, usize>> = HashMap::new();
 
+    for (line_num, line) in lines.iter().enumerate() {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        assert!(parts.len() == 5, "{} - bad line {line}", line_num + 1);
+        let val = usize::from_str_radix(parts[4], 10).unwrap();
+        let key = String::from(parts[0]);
+        let second = String::from(parts[2]);
+        if !hm.contains_key(&key) {
+            hm.insert(key.clone(), HashMap::new());
+        }
+        if !hm.contains_key(&second) {
+            hm.insert(second.clone(), HashMap::new());
+        }
+        hm.get_mut(&key).unwrap().insert(second.clone(), val);
+        hm.get_mut(&second).unwrap().insert(key.clone(), val);
+    }
+    for (k, v) in &hm {
+        println!("{k} -> {v:?}");
+    }
+    let perms = hm.keys().permutations(hm.len());
+    println!("permutations");
+    let mut res = Vec::new();
+    for p in perms {
+        let mut tot = 0;
+        for i in 0..p.len() - 1 {
+            tot += hm.get(p[i]).unwrap().get(p[i + 1]).unwrap();
+        }
+        res.push(tot);
+        println!("{p:?} - {tot}");
+    }
+    println!("min: {}", res.iter().min().unwrap());
     Ok(())
 }
