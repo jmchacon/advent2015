f126ebb474c0f89341a91b3b497708e018c75d2f
James Chacon <chacon.james@gmail.com>
2022-12-14 07:27:11 +0000

    day10 part1
    Implement look-and-say

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,29 +1,47 @@
 //! day10 advent 2022
 use clap::Parser;
 use color_eyre::eyre::Result;
-use slab_tree::tree::TreeBuilder;
-use std::collections::HashMap;
-use std::fs::File;
-use std::io;
-use std::io::BufRead;
-use std::path::Path;
+use std::str;
 
 #[derive(Parser)]
 #[command(author, version, about)]
 struct Args {
-    #[arg(long, default_value_t = String::from("input.txt"))]
-    filename: String,
+    #[arg(long, default_value_t = String::from("1113122113"))]
+    start: String,
+
+    #[arg(long, default_value_t = 40)]
+    rounds: usize,
 }
 
 fn main() -> Result<()> {
     color_eyre::install()?;
     let args: Args = Args::parse();
 
-    let filename = Path::new(env!("CARGO_MANIFEST_DIR")).join(args.filename);
-    let file = File::open(filename)?;
-    let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
-
-    for (line_num, line) in lines.iter().enumerate() {}
-
+    let mut cur = args.start.clone();
+    for i in 0..args.rounds {
+        //println!("{i} - {cur}");
+        let mut new = String::new();
+        let bytes = cur.as_str().as_bytes();
+        let mut prev = bytes[0];
+        let mut repeat = 1;
+        let mut cc: [u8; 1] = [0];
+        for c in &bytes[1..] {
+            if *c == prev {
+                repeat += 1;
+                continue;
+            }
+            cc[0] = prev;
+            let x = str::from_utf8(&cc).unwrap();
+            new += &format!("{repeat}{x}");
+            prev = *c;
+            repeat = 1;
+        }
+        cc[0] = prev;
+        let x = str::from_utf8(&cc).unwrap();
+        new += &format!("{repeat}{x}");
+        println!("{i} - {cur} -> {new}");
+        cur = new;
+    }
+    println!("len {}", cur.len());
     Ok(())
 }
