975a7c2b6f7587a732658bc8e2f5e748888e67f5
James Chacon <chacon.james@gmail.com>
2023-01-12 13:47:06 -0800

    day12 part1
    Deserialize json.
    
    Now walk the tree and add up any numbers found. Return total.
    
    Learned serde json a bit finally.

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,11 +1,9 @@
 //! day12 advent 2022
 use clap::Parser;
 use color_eyre::eyre::Result;
-use slab_tree::tree::TreeBuilder;
-use std::collections::HashMap;
+use serde_json::Value;
 use std::fs::File;
-use std::io;
-use std::io::BufRead;
+use std::io::{self, BufRead};
 use std::path::Path;
 
 #[derive(Parser)]
@@ -23,7 +21,38 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    let mut num = 0;
+    for (line_num, line) in lines.iter().enumerate() {
+        let Ok(v) = serde_json::from_str::<Value>(&line) else {
+            panic!("{} - bad line {line}", line_num+1);
+        };
 
+        num += total(&v);
+        println!("{v}");
+    }
+    println!("{num}");
     Ok(())
+}
+
+fn total(v: &Value) -> i64 {
+    match v {
+        Value::Null => 0,
+        Value::Bool(_) => 0,
+        Value::Number(v) => v.as_i64().unwrap(),
+        Value::String(_) => 0,
+        Value::Array(v) => {
+            let mut num = 0;
+            for val in v {
+                num += total(val);
+            }
+            num
+        }
+        Value::Object(m) => {
+            let mut num = 0;
+            for (_, v) in m {
+                num += total(v)
+            }
+            num
+        }
+    }
 }
