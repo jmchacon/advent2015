7ba68dbce80b2ea55e0c7d8411c9014efdbd3c0f
James Chacon <chacon.james@gmail.com>
2023-01-16 21:44:10 -0800

    day18 part 1
    Light shows with cellular atomata
    
    Iterate, then reduce the grid to a count of on lights.

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,18 +1,31 @@
 //! day18 advent 2022
 use clap::Parser;
 use color_eyre::eyre::Result;
-use slab_tree::tree::TreeBuilder;
-use std::collections::HashMap;
+use grid::{Grid, Location};
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
+use strum_macros::Display;
 
 #[derive(Parser)]
 #[command(author, version, about)]
 struct Args {
     #[arg(long, default_value_t = String::from("input.txt"))]
     filename: String,
+
+    #[arg(long, default_value_t = 100)]
+    rounds: usize,
+
+    #[arg(long, default_value_t = false)]
+    debug: bool,
+}
+
+#[derive(Clone, Debug, Default, Display, PartialEq, Eq)]
+enum Light {
+    On,
+    #[default]
+    Off,
 }
 
 fn main() -> Result<()> {
@@ -23,7 +36,71 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    let mut grid = Grid::<Light>::new(lines[0].len(), lines.len());
+    for (line_num, line) in lines.iter().enumerate() {
+        for (pos, b) in line.as_bytes().iter().enumerate() {
+            let l = Location(pos, line_num);
+            match b {
+                b'#' => grid.add(&l, Light::On),
+                b'.' => {}
+                _ => panic!("{} - bad line {line}", line_num + 1),
+            };
+        }
+    }
 
+    print_board(&grid);
+    println!();
+
+    for _ in 0..args.rounds {
+        step(&mut grid);
+        if args.debug {
+            print_board(&grid);
+            println!();
+        }
+    }
+
+    let on = grid.into_iter().filter(|x| *x == Light::On).count();
+    println!("{on} lights on after {} rounds", args.rounds);
     Ok(())
+}
+
+fn step(grid: &mut Grid<Light>) {
+    let mut newgrid = Grid::new(grid.width(), grid.height());
+    for y in 0..grid.height() {
+        for x in 0..grid.width() {
+            let l = Location(x, y);
+            let mut g = grid.get(&l).clone();
+            let n = grid
+                .neighbors_all(&l)
+                .iter()
+                .filter(|x| *x.1 == Light::On)
+                .count();
+            match g {
+                Light::On => {
+                    if n != 2 && n != 3 {
+                        g = Light::Off;
+                    }
+                }
+                Light::Off => {
+                    if n == 3 {
+                        g = Light::On;
+                    }
+                }
+            }
+            newgrid.add(&l, g);
+        }
+    }
+    *grid = newgrid;
+}
+
+fn print_board(grid: &Grid<Light>) {
+    for y in 0..grid.height() {
+        for x in 0..grid.width() {
+            match grid.get(&Location(x, y)) {
+                Light::On => print!("#"),
+                Light::Off => print!("."),
+            }
+        }
+        println!();
+    }
 }
