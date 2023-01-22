8f1e8b50d24c388d73768be2d523b6a0f5801c1d
James Chacon <chacon.james@gmail.com>
2023-01-21 23:20:26 -0800

    day20 part1/2
    I tried to implement by finding all the divisors but got off on the wrong track with a factor circle.
    
    That doesn't generate anything except the prime factors and I need all the factors.
    
    Cribbing from a solution this wasn't hard to generate them all.

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,29 +1,62 @@
 //! day20 advent 2022
 use clap::Parser;
 use color_eyre::eyre::Result;
-use slab_tree::tree::TreeBuilder;
-use std::collections::HashMap;
-use std::fs::File;
-use std::io;
-use std::io::BufRead;
-use std::path::Path;
 
 #[derive(Parser)]
 #[command(author, version, about)]
 struct Args {
     #[arg(long, default_value_t = String::from("input.txt"))]
     filename: String,
+
+    #[arg(long, default_value_t = 34000000)]
+    input: u64,
+
+    #[arg(long, default_value_t = 1)]
+    start: u64,
 }
 
 fn main() -> Result<()> {
     color_eyre::install()?;
     let args: Args = Args::parse();
 
-    let filename = Path::new(env!("CARGO_MANIFEST_DIR")).join(args.filename);
-    let file = File::open(filename)?;
-    let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
+    let (mut done1, mut done2) = (false, false);
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    for start in args.start.. {
+        let root = (start as f64).sqrt() as u64;
+        let mut div = Vec::new();
+        // Find all divisors up to the square root.
+        for j in 1..=root + 1 {
+            if start % j == 0 {
+                div.push(j);
+            }
+        }
+        let mut f = div.clone();
+        // For each one of the above divisors find the remaining
+        // multiples and add those in.
+        for d in div {
+            if d * d != start {
+                f.push(start / d);
+            }
+        }
+
+        let sum = f.iter().sum::<u64>() * 10;
+        let sum2 = f
+            .iter()
+            .map(|x| if start / x <= 50 { *x } else { 0 })
+            .sum::<u64>()
+            * 11;
+        if sum >= args.input && !done1 {
+            println!("{start} -> {sum}");
+            done1 = true;
+        }
+        if sum2 >= args.input && !done2 {
+            println!("part2 {start} => {sum}");
+            done2 = true;
+        }
+        if done1 && done2 {
+            break;
+        }
+    }
 
     Ok(())
 }
