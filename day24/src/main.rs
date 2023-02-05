e54de607da24efdba3180fe9869e5fbbd12554ba
James Chacon <chacon.james@gmail.com>
2023-02-04 22:01:17 -0800

    day24 part1
    Divide up N numbers 3 ways so their sums are all equal.
    
    Then pick the smallest one which has also the smallest product (if there are ties)

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,8 +1,7 @@
 //! day24 advent 2022
 use clap::Parser;
 use color_eyre::eyre::Result;
-use slab_tree::tree::TreeBuilder;
-use std::collections::HashMap;
+use itertools::Itertools;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
@@ -13,6 +12,12 @@ use std::path::Path;
 struct Args {
     #[arg(long, default_value_t = String::from("input.txt"))]
     filename: String,
+
+    #[arg(long, default_value_t = false)]
+    debug: bool,
+
+    #[arg(long, default_value_t = 3)]
+    buckets: u64,
 }
 
 fn main() -> Result<()> {
@@ -23,7 +28,39 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    let mut weights = Vec::new();
+    for line in &lines {
+        weights.push(line.parse::<u64>().unwrap());
+    }
+    let sum: u64 = weights.iter().sum();
+    let bucket_size = sum / args.buckets;
+    println!("Sum: {sum}");
+    assert!(
+        sum == bucket_size * args.buckets,
+        "Bad list, not equalable divisable"
+    );
 
+    let mut candidates = Vec::new();
+    for k in 2..=weights.len() - 2 {
+        for c in weights
+            .iter()
+            .cloned()
+            .combinations(k)
+            .filter(|x| x.iter().sum::<u64>() == bucket_size) // Find all combos which make equal the bucket size
+            .map(|x| (x.len(), x.iter().product::<u64>(), x))
+        // Return length of the vec, quantum entanglement and the vec (technically don't need but debugging..)
+        {
+            candidates.push(c);
+        }
+    }
+    // Sort it and the top one will be the one for compartment one with least QE
+    candidates.sort();
+    println!("Candidates: {}", candidates.len());
+    println!("1st: {:?}", candidates[0]);
+    if args.debug {
+        for c in &candidates {
+            println!("Candidate: {c:?}");
+        }
+    }
     Ok(())
 }
