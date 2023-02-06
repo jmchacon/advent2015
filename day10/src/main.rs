2b0d958bc9caf21afca1265231784b0edc5c5f0b
James Chacon <chacon.james@gmail.com>
2023-02-05 22:11:37 -0800

    Clippy cleanup. Standardize args/output

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -9,40 +9,48 @@ struct Args {
     #[arg(long, default_value_t = String::from("1113122113"))]
     start: String,
 
-    #[arg(long, default_value_t = 40)]
-    rounds: usize,
+    #[arg(long, default_value_t = false)]
+    debug: bool,
 }
 
 fn main() -> Result<()> {
     color_eyre::install()?;
     let args: Args = Args::parse();
 
-    let mut cur = args.start.clone();
-    for i in 0..args.rounds {
-        //println!("{i} - {cur}");
-        let mut new = String::new();
-        let bytes = cur.as_str().as_bytes();
-        let mut prev = bytes[0];
-        let mut repeat = 1;
-        let mut cc: [u8; 1] = [0];
-        for c in &bytes[1..] {
-            if *c == prev {
-                repeat += 1;
-                continue;
+    let rounds = [(1, 40), (2, 50)];
+
+    for r in &rounds {
+        let mut cur = args.start.clone();
+        for i in 0..r.1 {
+            if args.debug {
+                println!("{i} - {cur}");
+            }
+            let mut new = String::new();
+            let bytes = cur.as_str().as_bytes();
+            let mut prev = bytes[0];
+            let mut repeat = 1;
+            let mut cc: [u8; 1] = [0];
+            for c in &bytes[1..] {
+                if *c == prev {
+                    repeat += 1;
+                    continue;
+                }
+                cc[0] = prev;
+                let x = str::from_utf8(&cc).unwrap();
+                new += &format!("{repeat}{x}");
+                prev = *c;
+                repeat = 1;
             }
             cc[0] = prev;
             let x = str::from_utf8(&cc).unwrap();
             new += &format!("{repeat}{x}");
-            prev = *c;
-            repeat = 1;
+            if args.debug {
+                println!("{i} - {cur} -> {new}");
+                println!("{i}");
+            }
+            cur = new;
         }
-        cc[0] = prev;
-        let x = str::from_utf8(&cc).unwrap();
-        new += &format!("{repeat}{x}");
-        //println!("{i} - {cur} -> {new}");
-        println!("{i}");
-        cur = new;
+        println!("part{}: len {}", r.0, cur.len());
     }
-    println!("len {}", cur.len());
     Ok(())
 }
