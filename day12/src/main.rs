fff2be074f56859ee9564698ca043b59c2b99f80
James Chacon <chacon.james@gmail.com>
2023-02-05 22:25:00 -0800

    Clippy cleanup. Standardize args/output.

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -11,6 +11,9 @@ use std::path::Path;
 struct Args {
     #[arg(long, default_value_t = String::from("input.txt"))]
     filename: String,
+
+    #[arg(long, default_value_t = false)]
+    debug: bool,
 }
 
 fn main() -> Result<()> {
@@ -24,25 +27,25 @@ fn main() -> Result<()> {
     let mut num = 0;
     let mut num2 = 0;
     for (line_num, line) in lines.iter().enumerate() {
-        let Ok(v) = serde_json::from_str::<Value>(&line) else {
+        let Ok(v) = serde_json::from_str::<Value>(line) else {
             panic!("{} - bad line {line}", line_num+1);
         };
 
         num += total(&v, false);
         num2 += total(&v, true);
-        println!("{v}");
+        if args.debug {
+            println!("{v}");
+        }
     }
-    println!("{num}");
-    println!("{num2}");
+    println!("part1: {num}");
+    println!("part2: {num2}");
     Ok(())
 }
 
 fn total(v: &Value, ignore_red: bool) -> i64 {
     match v {
-        Value::Null => 0,
-        Value::Bool(_) => 0,
+        Value::Null | Value::String(_) | Value::Bool(_) => 0,
         Value::Number(v) => v.as_i64().unwrap(),
-        Value::String(_) => 0,
         Value::Array(vals) => vals.iter().map(|x| total(x, ignore_red)).sum(),
         Value::Object(m) => {
             if ignore_red && m.values().any(|x| x == "red") {
