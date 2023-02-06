b711829b5c5c74d7d16dfd8c4da156dd608cc46c
James Chacon <chacon.james@gmail.com>
2023-02-05 21:41:54 -0800

    Clippy cleanup and standardize args/output

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,23 +1,34 @@
 //! day2 advent 2015
+use clap::Parser;
 use color_eyre::eyre::Result;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
 
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Args {
+    #[arg(long, default_value_t = String::from("input.txt"))]
+    filename: String,
+}
+
 fn main() -> Result<()> {
-    let filename = Path::new(env!("CARGO_MANIFEST_DIR")).join("input.txt");
+    color_eyre::install()?;
+    let args: Args = Args::parse();
+
+    let filename = Path::new(env!("CARGO_MANIFEST_DIR")).join(args.filename);
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
     let mut total: u32 = 0;
     let mut ribbon: u32 = 0;
     for (line_num, line) in lines.iter().enumerate() {
-        let parts: Vec<&str> = line.split("x").collect();
+        let parts: Vec<&str> = line.split('x').collect();
         assert!(parts.len() == 3, "{} - bad line {line}", line_num + 1);
-        let l = u32::from_str_radix(parts[0], 10).unwrap();
-        let w = u32::from_str_radix(parts[1], 10).unwrap();
-        let h = u32::from_str_radix(parts[2], 10).unwrap();
+        let l = parts[0].parse::<u32>().unwrap();
+        let w = parts[1].parse::<u32>().unwrap();
+        let h = parts[2].parse::<u32>().unwrap();
         let areas = vec![l * w, w * h, h * l];
         total += areas.iter().map(|a| 2 * a).sum::<u32>();
         total += areas.iter().min().unwrap();
@@ -26,6 +37,6 @@ fn main() -> Result<()> {
         let vol = l * w * h;
         ribbon += perimeters.iter().min().unwrap() + vol;
     }
-    println!("total - {total}\nribbon - {ribbon}");
+    println!("part1: total - {total}\npart2: ribbon - {ribbon}");
     Ok(())
 }
