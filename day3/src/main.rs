e684e246d162405fd2b6ee6a9b61cf22da30b44c
James Chacon <chacon.james@gmail.com>
2023-02-05 21:47:54 -0800

    Clippy cleanup and use clap/standardize output.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,4 +1,5 @@
 //! day3 advent 2015
+use clap::Parser;
 use color_eyre::eyre::Result;
 use std::collections::HashSet;
 use std::fs::File;
@@ -6,49 +7,63 @@ use std::io;
 use std::io::BufRead;
 use std::path::Path;
 
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Args {
+    #[arg(long, default_value_t = String::from("input.txt"))]
+    filename: String,
+}
+
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 struct Location(i32, i32);
 
 fn main() -> Result<()> {
-    let filename = Path::new(env!("CARGO_MANIFEST_DIR")).join("input.txt");
+    color_eyre::install()?;
+    let args: Args = Args::parse();
+
+    let filename = Path::new(env!("CARGO_MANIFEST_DIR")).join(args.filename);
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    const DELIVERERS: usize = 2;
     for (line_num, line) in lines.iter().enumerate() {
-        let mut cur = Vec::new();
-        let mut hm = HashSet::new();
-        for _ in 0..DELIVERERS {
-            cur.push(Location(0, 0));
-            hm.insert(Location(0, 0));
-        }
-        for (pos, e) in line.bytes().enumerate() {
-            let ind = pos % DELIVERERS;
-            match e {
-                b'>' => {
-                    cur[ind] = Location(cur[ind].0 + 1, cur[ind].1);
-                }
-                b'^' => {
-                    cur[ind] = Location(cur[ind].0, cur[ind].1 + 1);
-                }
-                b'v' => {
-                    cur[ind] = Location(cur[ind].0, cur[ind].1 - 1);
-                }
-                b'<' => {
-                    cur[ind] = Location(cur[ind].0 - 1, cur[ind].1);
-                }
-                _ => {
-                    panic!(
-                        "{} - bad line. bad char {e} at pos {} - {line}",
-                        line_num + 1,
-                        pos + 1
-                    );
-                }
-            }
-            hm.insert(cur[ind].clone());
-        }
-        println!("Visited {} houses", hm.len());
+        println!("part1: Visited {} houses", deliveries(1, line, line_num));
+        println!("part2: Visited {} houses", deliveries(2, line, line_num));
     }
 
     Ok(())
+}
+
+fn deliveries(deliverers: usize, line: &str, line_num: usize) -> usize {
+    let mut cur = Vec::new();
+    let mut hm = HashSet::new();
+    for _ in 0..deliverers {
+        cur.push(Location(0, 0));
+        hm.insert(Location(0, 0));
+    }
+    for (pos, e) in line.bytes().enumerate() {
+        let ind = pos % deliverers;
+        match e {
+            b'>' => {
+                cur[ind] = Location(cur[ind].0 + 1, cur[ind].1);
+            }
+            b'^' => {
+                cur[ind] = Location(cur[ind].0, cur[ind].1 + 1);
+            }
+            b'v' => {
+                cur[ind] = Location(cur[ind].0, cur[ind].1 - 1);
+            }
+            b'<' => {
+                cur[ind] = Location(cur[ind].0 - 1, cur[ind].1);
+            }
+            _ => {
+                panic!(
+                    "{} - bad line. bad char {e} at pos {} - {line}",
+                    line_num + 1,
+                    pos + 1
+                );
+            }
+        }
+        hm.insert(cur[ind].clone());
+    }
+    hm.len()
 }
